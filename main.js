238e51b711949e82c441572a1da90d58a9f05935
NickW16 <171855220+NickW16@users.noreply.github.com>
2025-07-03 16:59:10 -0300

    Complete primitive search tree, still optimizations to do

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// test array
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const uniqueSortedArray = [...new Set(testArray)].sort((a, b) => a - b);
+
+// node factory with both right and left attributes
 function Node(data) {
     this.data = data;
     this.left = null;
@@ -5,17 +10,38 @@ function Node(data) {
 }
 
 function Tree(array) {
-    let root;
+    this.root = buildTree(array, 0, array.length - 1);
 }
 
-function buildTree(array) {
-    let start = 0;
-    let end = array.length - 1;
-
+function buildTree(array, start, end) { // for sorted arrays
     if (start > end) return null;
 
+    // calculate mid node
     let mid = start + Math.floor((end - start) / 2);
+    // create new root
+    let root = new Node(array[mid]);
 
-    let root = 
+    // find left and right subtree
+    root.left = buildTree(array, start, mid - 1);
+    root.right = buildTree(array, mid + 1, end);
 
+    return root;
 }
+
+const myTree = new Tree(uniqueSortedArray);
+
+// pretty print function from theodinproject
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+  }
+  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+};
+// prints the binary search tree
+prettyPrint(myTree.root);
