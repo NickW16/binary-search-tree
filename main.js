e6c14da1bef76c65e11e2a538b5a3f6babe8069e
NickW16 <171855220+NickW16@users.noreply.github.com>
2025-07-03 16:26:47 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+function Node(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+}
+
+function Tree(array) {
+    let root;
+}
+
+function buildTree(array) {
+    let start = 0;
+    let end = array.length - 1;
+
+    if (start > end) return null;
+
+    let mid = start + Math.floor((end - start) / 2);
+
+    let root = 
+
+}
